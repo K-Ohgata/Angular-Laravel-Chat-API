99273b7d03ab29578c504e2a2c1f78aa77f41d66
K-Ohgata <oh.kazuma1017@gmail.com>
2022-01-25 17:48:33 +0900

    cors user changed

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', 'http://localhost:4200')
+        ->header('Access-Control-Allow-Origin', 'https://goofy-ptolemy-6da173.netlify.app')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Content-Type');
     }
